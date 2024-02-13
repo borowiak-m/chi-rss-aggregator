6c71a955f4f311dcb121844da19f05750183c580
Mariusz Borowiak <142698842+borowiak-m@users.noreply.github.com>
2024-02-13 23:57:44 +0000

    Added fetching feeds and scraping into posts table func

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/borowiak-m/rss-aggregator/internal/database"
 	"github.com/go-chi/chi"
@@ -36,9 +37,12 @@ func main() {
 		log.Fatal("Can't connect to db")
 	}
 
+	db := database.New(conn)
 	apiCfg := apiConfig{
-		DB: database.New(conn),
+		DB: db,
 	}
+
+	go startScraping(db, 10, time.Minute)
 
 	router := chi.NewRouter()
 
